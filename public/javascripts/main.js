e7b732b9dc54fadd882e3479a266635e6d7f9680
xandramiyo <aj.sumiyoshi@gmail.com>
2023-01-31 20:48:47 -0800

    hover and sub menu for threads now works. styling needs work but is functional. edit function still not available

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,20 +1,37 @@
 console.log('Hello World')
 
-const commentCtr = document.querySelector('.post-comment-ctr')
-const commentMenu = document.querySelector('.comment-menu-btn')
+//nav bar submenu
+const userDropdown = document.querySelector('.user-menu-dropdown')
+const userMenu = document.querySelector('.user-menu-wrap')
 
-commentCtr.addEventListener('click', handleClick)
+userDropdown.addEventListener('click', function(evt) {
+  userMenu.classList.toggle('open-menu')
+})
 
-function handleClick(evt) {
-  if(evt.target.classList.contains('btn-img')) {
-    const showPostCommentDiv = evt.target.parentNode.parentNode
-    showPostCommentDiv.querySelector('.sub-menu').classList.toggle('hidden')
+//thread submenu
+const threadCtr = document.querySelector('.thread-ctr')
+
+if(threadCtr) {
+  threadCtr.addEventListener('click', threadMenuClick)
+
+  function threadMenuClick(evt) {
+    const threadBanner = evt.target.parentNode.parentNode
+    threadBanner.querySelector('.dot-menu').classList.toggle('hidden')
   }
 }
 
-const nav = document.querySelector(".nav")
+//comment submenu
+const commentCtr = document.querySelector('.post-comment-ctr')
+const commentMenu = document.querySelector('.comment-menu-btn')
 
-nav.addEventListener('click', function(evt) {
-  console.log('click')
-})
+if(commentCtr) {
+  commentCtr.addEventListener('click', handleClick)
+  
+  function handleClick(evt) {
+    if(evt.target.classList.contains('btn-img')) {
+      const showPostCommentDiv = evt.target.parentNode.parentNode
+      showPostCommentDiv.querySelector('.sub-menu').classList.toggle('hidden')
+    }
+  }
+}
 
